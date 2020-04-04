9c25872fe1ba9107efebf454b980381b39b05884
Lalit J <lalit.jhandai@gmail.com>
2020-04-04 13:48:01 +0530

    18. FutureBuilders and StreamBuilders for Fetching and Streaming Data

--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -1,8 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttershare/widgets/header.dart';
+import 'package:fluttershare/widgets/progress.dart';
 
-final userRef = Firestore.instance.collection('users');
+final usersRef = Firestore.instance.collection('users');
 
 class Timeline extends StatefulWidget {
   @override
@@ -11,14 +12,14 @@ class Timeline extends StatefulWidget {
 
 class _TimelineState extends State<Timeline> {
   @override
-  void initState() { 
+  void initState() {
     //getUsers();
-    getUserById();
+    //getUserById();
     super.initState();
   }
 
   getUsers(){
-    userRef.getDocuments().then((QuerySnapshot snapshot){
+    usersRef.getDocuments().then((QuerySnapshot snapshot){
       snapshot.documents.forEach((DocumentSnapshot doc){
         print(doc.data);
         print(doc.data['username']);
@@ -30,17 +31,33 @@ class _TimelineState extends State<Timeline> {
 
   getUserById() async{
     final String id = "dV14NzYK0pnBjAnvh062";
-    final DocumentSnapshot doc = await userRef.document(id).get();
+    final DocumentSnapshot doc = await usersRef.document(id).get();
     print(doc.data);
     print(doc.data['username']);
     print(doc.documentID);
     print(doc.exists);
   }
 
+  @override
   Widget build(context) {
     return Scaffold(
       appBar: header(context, isAppTitle: true),
-      body: Text("Timeline"),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: usersRef.snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return circularProgress();
+          }
+          final List<Widget> children = snapshot.data.documents
+              .map((doc) => Text(doc['username']))
+              .toList();
+          return Container(
+            child: ListView(
+              children: children,
+            ),
+          );
+        },
+      ),
     );
   }
 }
